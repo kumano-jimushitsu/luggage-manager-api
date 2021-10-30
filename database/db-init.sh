6e8b47df179fb385ff08d59ad2a4b5dec14f464e
i11u <incredibleteru@gmail.com>
2021-10-31 00:36:40 +0900

    Files related to docker are removed

--- database/db-init.sh
+++
@@ -1,7 +0,0 @@
-#start SQL Server, start the script to create/setup the DB
- #wait for the SQL Server to come up
-sleep 15s
-
-echo "running set up script"
-#run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P My-Secret-Pwd-1 -d master -i db-init.sql
